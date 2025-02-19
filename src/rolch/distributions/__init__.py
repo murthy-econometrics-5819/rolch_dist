d3a54b3797a2493bc8ee97f6084acd16628e7121
murthy-econometrics-5819 <120830181+murthy-econometrics-5819@users.noreply.github.com>
2025-02-19 21:05:34 +0100

    Update __init__.py

--- src/rolch/distributions/__init__.py
+++ src/rolch/distributions/__init__.py
@@ -3,11 +3,13 @@ from .johnsonsu import DistributionJSU
 from .normal import DistributionNormal
 from .studentt import DistributionT
 from .exponential import DistributionExponential
+from .beta import DistributionBeta
 
 __all__ = [
     "DistributionNormal",
     "DistributionT",
     "DistributionJSU",
     "DistributionGamma",
-    "DistributionExponential"
+    "DistributionExponential",
+    "DistributionBeta"
 ]
